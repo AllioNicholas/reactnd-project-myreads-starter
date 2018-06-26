557a8e4724c59c362ed91d35d67d55fb31beb129
Nicholas <nicholas@ulmon.com>
2018-06-26 16:17:20 +0200

    Added link to open search

--- src/MyReadsBookShelves.js
+++ src/MyReadsBookShelves.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Bookshelf from './Bookshelf'
+import { Link } from 'react-router-dom'
 
 class MyReadsBookShelves extends Component {
   static propTypes = {
@@ -23,7 +24,7 @@ class MyReadsBookShelves extends Component {
           </div>
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link to='/search' >Add a book</Link>
         </div>
       </div>
     )
