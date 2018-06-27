97e5d3d2a5a42fbbe0eaf074de287e33ab772cfb
Nicholas <nicholas@ulmon.com>
2018-06-27 13:13:30 +0200

    Added link to return to home page

--- src/MyReadsSearchPage.js
+++ src/MyReadsSearchPage.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+import BookList from './BookList'
 
 class MyReadsSearchPage extends Component {
+
+  componentDidMount() {
+
+  }
+
   render() {
     return(
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <Link to='/' className="close-search" >Close</Link>
           <div className="search-books-input-wrapper">
             {/*
               NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -21,7 +29,7 @@ class MyReadsSearchPage extends Component {
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          {/* <BookList /> */}
         </div>
       </div>
     )
